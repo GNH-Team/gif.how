727e7d5366ac5067013fafd8512bdb5ec7ccba99
Hoang Lam <xuannt@gnhteam.com>
2025-03-10 15:18:24 +0700

    feat: update init script for improved service setup

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 # Description:
 # 1. Starts MariaDB and imports SQL dump
 # 2. Starts remaining services (excluding sync-service)
-# 3. Executes bun commands inside the sync-service directory
+# 3. Executes bun commands inside the sync-service directory using a temporary DATABASE_URL override
 # 4. Shuts down docker compose services when complete
 # ============================================
 
@@ -33,7 +33,7 @@ sleep 5
 echo "[INFO] Changing directory to sync-service..."
 cd "./sync-service"
 
-echo "[INFO] Running: bun install & copy .env file to sync-service directory"
+echo "[INFO] Running: bun install & setting up .env file"
 bun install
 sed -n '/# \* SYNC-SERVICE/,$p' ../.env > .env
 if [ $? -ne 0 ]; then
@@ -44,15 +44,12 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-
-
 # Check if /prisma directory exists before running bun p:init
 if [ -d "prisma" ]; then
     echo "[INFO] Prisma directory already exists, skipping bun p:init..."
 else
-    echo "[INFO] Running: bun p:init"
-    bun prisma init --datasource-provider=mysql --url=$(grep '^DATABASE_URL=' .env | cut -d '=' -f2- | sed 's/@mariadb/@localhost/')
-
+    echo "[INFO] Running: bun p:init with temporary DATABASE_URL override"
+    bun p:init
     if [ $? -ne 0 ]; then
         echo "[ERROR] bun p:init failed."
         echo "[INFO] Shutting down containers..."
@@ -61,10 +58,9 @@ else
         exit 1
     fi
 
-
-
     # Set the schema file name (adjust path if needed)
     schema_file_name="prisma/schema.prisma"
+  
 
     # Check if binaryTargets is already present in the prisma schema.
     if ! grep -q "binaryTargets" "$schema_file_name"; then
@@ -72,24 +68,28 @@ else
         sed -i '/^generator client {/,/^}/ { /provider =/a\
           binaryTargets = ["native","debian-openssl-1.1.x"]
           }' "$schema_file_name"
-
         if [ $? -ne 0 ]; then
             echo "[ERROR] Failed to update binaryTargets in schema file."
             echo "[INFO] Shutting down containers..."
             exit 1
         fi
-            else
+    else
         echo "[INFO] binaryTargets already present in the schema, skipping insertion."
     fi
 fi
 
-echo "[INFO] Running: bun p:sync"
-cp ../.env .env.production
-bun p:sync
-
-cd ..
+echo "[INFO] Running: bun p:sync with temporary DATABASE_URL override"
+temp_db_url=$(grep '^DATABASE_URL=' .env | cut -d '=' -f2- | sed 's/@mariadb/@localhost:10002/')
+DATABASE_URL=$temp_db_url bun p:sync
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Failed to run bun p:sync."
+    echo "[INFO] Shutting down containers..."
+    docker compose down
+    exit 1
+fi
 
 echo "[INFO] Build sync-service image..."
+cd ..
 docker compose build
 if [ $? -ne 0 ]; then
     echo "[ERROR] Failed to build sync-service image."
